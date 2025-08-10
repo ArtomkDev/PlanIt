f16ff31a5afb4105342add4274956a05f7552c77
ArtomkDev <artomk.dev@gmail.com>
2025-08-10 20:28:11 +0300

    зберігання сеансу

--- firebase.js
+++ firebase.js
@@ -1,32 +1,40 @@
 // firebase.js
-import { getAnalytics } from 'firebase/analytics' // Для аналітики
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth' // Для автентифікації
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
 import {
-    doc,
-    getDoc,
-    getFirestore,
-    onSnapshot,
-    setDoc,
-    updateDoc,
-} from 'firebase/firestore'
+    getAuth,
+    initializeAuth,
+    getReactNativePersistence,
+    browserLocalPersistence
+} from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Конфігурація Firebase з нового проекту
+// Конфіг Firebase
 const firebaseConfig = {
-    apiKey: 'AIzaSyD7PcSEZkYGtgTw2fWE1Q0dtD0svw-nua8',
-    authDomain: 'errn-cacb3.firebaseapp.com',
-    projectId: 'errn-cacb3',
-    storageBucket: 'errn-cacb3.firebasestorage.app',
-    messagingSenderId: '66089248812',
-    appId: '1:66089248812:web:05d553816b8f701b038989',
-    measurementId: 'G-04JWLXNTSE',
+    apiKey: "AIzaSyD7PcSEZkYGtgTw2fWE1Q0dtD0svw-nua8",
+    authDomain: "errn-cacb3.firebaseapp.com",
+    projectId: "errn-cacb3",
+    storageBucket: "errn-cacb3.firebasestorage.app",
+    messagingSenderId: "66089248812",
+    appId: "1:66089248812:web:05d553816b8f701b038989",
+    measurementId: "G-04JWLXNTSE"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+let auth;
+
+// Якщо працюємо в браузері (Web)
+if (typeof window !== "undefined" && typeof document !== "undefined") {
+    auth = getAuth(app);
+    auth.setPersistence(browserLocalPersistence);
+} 
+// Якщо React Native (iOS / Android)
+else {
+    auth = initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage)
+    });
 }
 
-// Ініціалізація Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app) // Аналітика
-const db = getFirestore(app) // Firestore
-const auth = getAuth(app) // Автентифікація
-
-// Експорт модулів
-export { analytics, app, auth, db, doc, getDoc, onSnapshot, setDoc, updateDoc }
+export { app, auth, db, doc, getDoc, onSnapshot, setDoc, updateDoc };
